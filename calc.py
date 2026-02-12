e7654db1b9e0e1bfe9bb6509f624e083cd96381e
Jorgette <169021834+jorgettee@users.noreply.github.com>
2026-02-12 13:42:05 +0800

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,11 +1,2 @@
 def add(a, b):
     return a + b
-
-
-if __name__ == "__main__":
-    result = add(5, 7)
-    print(f"Checking 5 + 7... Result: {result}")
-
-    # A simple assertion to make the script fail if the math is wrong
-    assert result == 12, "Math error: 5 + 7 should be 12"
-    print("Test passed!")
