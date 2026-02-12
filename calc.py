8a2408103d664283aab87a6c9391b21220895c76
Jorgette <169021834+jorgettee@users.noreply.github.com>
2026-02-12 14:18:53 +0800

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,4 +1,9 @@
 def add(a, b):
-    return a - b
+    return a + b
 
+
+if __name__ == "__main__":
+    # This runs when we call 'python calc.py'
+    result = add(5, 7)
+    print(f"Result of 5 + 7 is: {result}")
     assert result == 12, "Calculation Error!"
